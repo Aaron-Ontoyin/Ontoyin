ab6984929b808c2f623865838ec26f531a89b57e
Macbook Pro <macbookpro@Edmond-Yin.local>
2022-02-08 17:20:47 +0000

    Corrected staticfiles and added whitenoise

--- ontoyin/settings.py
+++ ontoyin/settings.py
@@ -120,12 +120,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/base/styles')
 
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static/base/styles')
+    os.path.join(BASE_DIR, 'static')
 ]
 
 MEDIAFILES_DIRS = ''
